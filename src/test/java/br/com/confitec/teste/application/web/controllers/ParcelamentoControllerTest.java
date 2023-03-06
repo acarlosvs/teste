413961d8180aef36e43559a03675cadc671f8b42
Antonio Carlos <antoniocarlos_vs@hotmail.com>
2023-03-06 18:31:46 -0300

    Adicionando testes com MockMvc para controller e adicionando teste de erro na validação ao chamar endpoint /parcelamento

--- src/test/java/br/com/confitec/teste/application/web/controllers/ParcelamentoControllerTest.java
+++ src/test/java/br/com/confitec/teste/application/web/controllers/ParcelamentoControllerTest.java
@@ -6,33 +6,43 @@ import br.com.confitec.teste.commons.dto.request.SeguroDTO;
 import br.com.confitec.teste.commons.dto.response.DadosDTO;
 import br.com.confitec.teste.commons.dto.response.ParcelamentoDTO;
 import br.com.confitec.teste.services.ParcelamentoService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(ParcelamentoController.class)
 class ParcelamentoControllerTest {
 
-    @Mock
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
     private ParcelamentoService parcelamentoService;
 
-    @InjectMocks
-    private ParcelamentoController parcelamentoController;
-
     @Test
-    void deveRetornarParcelamento() {
+    @SneakyThrows
+    void deveParcelar() {
+        BigDecimal valorCobertura = BigDecimal.valueOf(50).setScale(2, RoundingMode.HALF_EVEN);
         List<CoberturaDTO> coberturasLista = new ArrayList<>();
-        coberturasLista.add(new CoberturaDTO(1, BigDecimal.valueOf(50)));
+        coberturasLista.add(new CoberturaDTO(1, valorCobertura));
 
         List<OpcaoParcelamentoDTO> opcaoParcelamentoLista = new ArrayList<>();
         opcaoParcelamentoLista.add(new OpcaoParcelamentoDTO(1, 1, BigDecimal.ZERO));
@@ -45,8 +55,35 @@ class ParcelamentoControllerTest {
 
         when(parcelamentoService.criarPlanoParcelaemento(seguroDTO)).thenReturn(parcelamentoDTO);
 
-        ResponseEntity<ParcelamentoDTO> actual = parcelamentoController.parcelar(seguroDTO);
+        mockMvc.perform(post("/parcelamento")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(seguroDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.dados[0].quantidadeParcelas").value(1))
+                .andExpect(jsonPath("$.dados[0].valorParcelamentoTotal").value( BigDecimal.valueOf(500)));
+    }
 
-        assertEquals(parcelamentoDTO, actual.getBody());
+    @Test
+    @SneakyThrows
+    void deveRetornarErroDeValidacao() {
+        BigDecimal valorCobertura = BigDecimal.valueOf(50).setScale(2, RoundingMode.HALF_EVEN);
+        List<CoberturaDTO> coberturasLista = new ArrayList<>();
+        coberturasLista.add(new CoberturaDTO(1, valorCobertura));
+
+        List<OpcaoParcelamentoDTO> opcaoParcelamentoLista = new ArrayList<>();
+        opcaoParcelamentoLista.add(new OpcaoParcelamentoDTO(0, 1, BigDecimal.ZERO));
+
+        SeguroDTO seguroDTO = new SeguroDTO(coberturasLista, opcaoParcelamentoLista);
+
+        List<DadosDTO> dados = new ArrayList<>();
+        dados.add(new DadosDTO(1, BigDecimal.valueOf(500), BigDecimal.valueOf(500), BigDecimal.valueOf(500)));
+        ParcelamentoDTO parcelamentoDTO = new ParcelamentoDTO(dados);
+
+        when(parcelamentoService.criarPlanoParcelaemento(seguroDTO)).thenReturn(parcelamentoDTO);
+
+        mockMvc.perform(post("/parcelamento")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(seguroDTO)))
+                .andExpect(status().isBadRequest());
     }
 }
